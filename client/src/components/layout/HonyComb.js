fd79bcd4887e482f77c7c29810afca70bb4049e9
Johns777-susana <senma777@gmail.com>
2022-01-03 18:42:15 +0545

    honycomb completion

--- client/src/components/layout/HonyComb.js
+++ client/src/components/layout/HonyComb.js
@@ -11,103 +11,187 @@ import Mobile from '../../assets/honeycomb/mobile.svg';
 import Money from '../../assets/honeycomb/money.svg';
 import StudentReading from '../../assets/honeycomb/student-reading.svg';
 import Website from '../../assets/honeycomb/website.svg';
+import Attendance from '../../assets/honeycomb/attendance.svg';
+import Bus from '../../assets/honeycomb/bus.svg';
+import Sms from '../../assets/honeycomb/sms.svg';
 
 const HonyComb = () => {
   return (
     <div
       style={{
         paddingBottom: '8rem',
+        height: '50%',
       }}
     >
       <ul className='honeycomb'>
         <li className='honeycomb-cell'>
-          <div className='honeycomb-background'></div>
-          <div>
+          <div
+            className='honeycomb-background'
+            style={{
+              backgroundColor: '#354DF4',
+            }}
+          ></div>
+          <div className='honeycomb-details'>
             <img src={Calendar} alt='' />
             <p className='honeycomb-text'>Academic Calendar</p>
           </div>
         </li>
         <li className='honeycomb-cell'>
-          <div className='honeycomb-background'></div>
-          <div>
+          <div
+            className='honeycomb-background'
+            style={{
+              backgroundColor: '#007ee9',
+            }}
+          ></div>
+          <div className='honeycomb-details'>
             <img src={Class} alt='' />
             <p className='honeycomb-text'>Grade/Class Management</p>
           </div>
         </li>
         <li className='honeycomb-cell'>
-          <div className='honeycomb-background'></div>
+          <div
+            className='honeycomb-background'
+            style={{ backgroundColor: '#008fec' }}
+          ></div>
           <div>
             <img src={Company} alt='' />
             <p className='honeycomb-text'>Rolewise Permission Login</p>
           </div>
         </li>
         <li className='honeycomb-cell'>
-          <div className='honeycomb-background'></div>
+          <div
+            className='honeycomb-background'
+            style={{
+              backgroundColor: '#00b7f6',
+            }}
+          ></div>
           <div>
             <img src={Files} alt='' />
-            <p className='honeycomb-text'>Grade/CAS Mangement</p>
+            <p className='honeycomb-text'>Daily Lesson Plan Mangement</p>
           </div>
         </li>
         <li className='honeycomb-cell'>
-          <div className='honeycomb-background'></div>
+          <div
+            className='honeycomb-background'
+            style={{
+              backgroundColor: '#00e6f6',
+            }}
+          ></div>
           <div>
             <img src={Form} alt='' />
             <p className='honeycomb-text'>Subject Management</p>
           </div>
         </li>
         <li className='honeycomb-cell'>
-          <div className='honeycomb-background'></div>
+          <div
+            className='honeycomb-background'
+            style={{
+              backgroundColor: '#0053f5',
+            }}
+          ></div>
           <div>
             <img src={GradeSheet} alt='' />
             <p className='honeycomb-text'>Grade/CAS management</p>
           </div>
         </li>
         <li className='honeycomb-cell'>
-          <div className='honeycomb-background'></div>
+          <div
+            className='honeycomb-background'
+            style={{
+              backgroundColor: '#0060ed',
+            }}
+          ></div>
           <div>
             <img src={Info} alt='' />
             <p className='honeycomb-text'>Notice Managment</p>
           </div>
         </li>
+      </ul>
+      <ul className='honeycomb'>
         <li className='honeycomb-cell'>
-          <div className='honeycomb-background'></div>
+          <div
+            className='honeycomb-background'
+            style={{
+              backgroundColor: '#008fec',
+            }}
+          ></div>
           <div>
             <img src={Meeting} alt='' />
             <p className='honeycomb-text'>Employee Information System</p>
           </div>
         </li>
         <li className='honeycomb-cell'>
-          <div className='honeycomb-background'></div>
-          <div>
+          <div
+            className='honeycomb-background'
+            style={{
+              backgroundColor: '#00cff9',
+            }}
+          ></div>
+          <div className='honeycomb-details'>
             <img src={Mobile} alt='' />
-            <p className='honeycomb-text'>
-              Leave Application & Approval System
-            </p>
+            <p className='honeycomb-text'>Leave Approval System</p>
           </div>
         </li>
-      </ul>
-      <ul className='honeycomb'>
         <li className='honeycomb-cell'>
-          <div className='honeycomb-background'></div>
+          <div
+            className='honeycomb-background'
+            style={{
+              backgroundColor: '#002dff',
+            }}
+          ></div>
           <div>
             <img src={Money} alt='' />
             <p className='honeycomb-text'>Banking Transaction Facilities</p>
           </div>
         </li>
         <li className='honeycomb-cell'>
-          <div className='honeycomb-background'></div>
-          <div>
+          <div
+            className='honeycomb-background'
+            style={{
+              backgroundColor: '#0043fb',
+            }}
+          ></div>
+          <div className='honeycomb-details'>
             <img src={StudentReading} alt='' />
-            <p className='honeycomb-text'>
-              Student Information And Management system
-            </p>
+            <p className='honeycomb-text'>Student Management system</p>
+          </div>
+        </li>
+      </ul>
+      <ul className='honeycomb'>
+        <li className='honeycomb-cell'>
+          <div
+            className='honeycomb-background'
+            style={{
+              backgroundColor: '#009eec',
+            }}
+          ></div>
+          <div>
+            <img src={Attendance} alt='' />
+            <p className='honeycomb-text'>E-Attendance System</p>
           </div>
         </li>
         <li className='honeycomb-cell'>
-          <div className='honeycomb-background'></div>
+          <div
+            className='honeycomb-background'
+            style={{
+              backgroundColor: '#0BB8FA',
+            }}
+          ></div>
           <div>
             <img src={Website} alt='' />
-            <p className='honeycomb-text'>E-Attendance System</p>
+            <p className='honeycomb-text'>Addmission Portal</p>
+          </div>
+        </li>
+        <li className='honeycomb-cell'>
+          <div
+            className='honeycomb-background'
+            style={{
+              backgroundColor: '#1F87EB',
+            }}
+          ></div>
+          <div>
+            <img src={Bus} alt='' />
+            <p className='honeycomb-text'>Transportation System</p>
           </div>
         </li>
       </ul>
